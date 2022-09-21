0c5ecef94be3eaa522bf205c800b0e67757cb191
Furkan Cömert <frkncmrt61@gmail.com>
2022-09-21 20:59:28 +0300

    First Commit

---
+++ datatypes/src/datatypes/Main.java
@@ -0,0 +1,19 @@
+package datatypes;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int sayi = 12;
+		sayi = 11;
+
+		double sayi2 = 12.5;
+		sayi = -129;
+
+		char karakter = 'A';
+
+		boolean dogruMu = true;
+		boolean yanlısMı = false;
+
+	}
+
+}
---
+++ variables/src/variables/Main.java
@@ -0,0 +1,19 @@
+package variables;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// reusability
+		int sayi = 15;
+		String mesaj = "Öğrenci sayısı : ";
+		System.out.println(mesaj + sayi);
+
+		/*
+		 * System.out.println("Öğrenci sayım : 9");
+		 * System.out.println("Öğrenci sayım : 9"); kötü kullanım
+		 * System.out.println("Öğrenci sayım : 9");
+		 */
+
+	}
+
+}
